62874cd7794066ee89f51e7834c24eb209a41888
Petrache Andrei Liviu <petrache.andrei1@gmail.com>
2023-04-17 16:55:24 +0300

    It does not work and idk what am I doing wrong

--- lib/widgets/image_with_input.dart
+++ lib/widgets/image_with_input.dart
@@ -1,26 +1,7 @@
-import 'dart:collection';
-import 'dart:io';
-
-import 'package:flutter/src/widgets/framework.dart';
-import 'package:flutter/src/widgets/placeholder.dart';
-import 'package:path_provider/path_provider.dart';
-
-List file = [];
-void _listofFiles() async {
-  var directory = (await getApplicationDocumentsDirectory()).path;
-  file = Directory("/assets/logos")
-      .listSync(); //use your folder name insted of resume.
-  file.forEach((element) {
-    print(element);
-  });
-}
+import 'package:flutter/material.dart';
 
 class ImageWithInput extends StatefulWidget {
-  final logos = <String, String>{};
-  ImageWithInput({super.key}) {
-    _listofFiles();
-  }
-
+  const ImageWithInput({super.key});
   @override
   State<ImageWithInput> createState() => _ImageWithInputState();
 }
@@ -28,6 +9,6 @@ class ImageWithInput extends StatefulWidget {
 class _ImageWithInputState extends State<ImageWithInput> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Placeholder();
   }
 }
