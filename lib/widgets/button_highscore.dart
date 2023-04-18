c5e89ca36fffc42fc6222ccf7ecc7b0a2727574f
Petrache Andrei Liviu <petrache.andrei1@gmail.com>
2023-04-18 20:17:22 +0300

    Added custom stateful button to display the highscore. it updates once/second

---
+++ lib/widgets/button_highscore.dart
@@ -0,0 +1,77 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+class HighscoreButton extends StatefulWidget {
+  const HighscoreButton({super.key});
+
+  @override
+  State<HighscoreButton> createState() => _HighscoreButtonState();
+}
+
+class _HighscoreButtonState extends State<HighscoreButton> {
+  // initialising the highscore
+  int _highscore = 998;
+
+  // timer to update the highscore
+  late Timer timer;
+
+  @override
+  void initState() {
+    super.initState();
+    _loadHighscore();
+    timer = Timer.periodic(const Duration(seconds: 1), (_) {
+      setState(() {
+        _loadHighscore();
+      });
+    });
+  }
+
+  // Loading the highscore on start
+  Future<void> _loadHighscore() async {
+    final prefs = await SharedPreferences.getInstance();
+    setState(() {
+      _highscore = prefs.getInt('highscore') ?? -10;
+    });
+  }
+
+  // reset highscore on button press
+  Future<void> _resetHighscore() async {
+    final prefs = await SharedPreferences.getInstance();
+    setState(() {
+      prefs.setInt('highscore', 0);
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return FloatingActionButton.extended(
+      key: UniqueKey(),
+      heroTag: 'highScore',
+      label: Text(
+        'Your highscore is $_highscore',
+      ),
+      backgroundColor: const Color(0xffe5e5e5),
+      foregroundColor: const Color(0xff003049),
+      shape: const RoundedRectangleBorder(
+        borderRadius: BorderRadius.all(Radius.circular(15.0)),
+      ),
+      icon: const Icon(
+        Icons.arrow_upward_outlined,
+        size: 24.0,
+        color: Colors.blueAccent,
+      ),
+      onPressed: () {
+        _resetHighscore();
+      },
+    );
+  }
+
+  // used to free the timer
+  @override
+  void dispose() {
+    super.dispose();
+    timer.cancel();
+  }
+}
